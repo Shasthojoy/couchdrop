54b2075c52616edf267e0340804bc733a05067e6
Michael Lawson <michael.lawson@linewize.com>
2017-07-01 19:44:55 +1200

    ..

--- endpoints/ssh/src/test/java/SshWorkerTestCase.java
+++ endpoints/ssh/src/test/java/SshWorkerTestCase.java
@@ -5,13 +5,13 @@ import org.apache.sshd.SshServer;
  * Created by michaellawson on 12/06/16
  */
 
-public class SshWorkerTestCase extends TestCase {
-    public void testName() throws Exception {
-        io.couchdrop.endpoints.ssh.SshWorker serverb = new io.couchdrop.endpoints.ssh.SshWorker();
-        SshServer sshServer = serverb.createSshServer(5025, "", "/tmp", "http://localhost:5088", "fake_authentication_token");
-        sshServer.start();
-        while (true) {
-            Thread.sleep(4000);
-        }
-    }
-}
+//public class SshWorkerTestCase extends TestCase {
+////    public void testName() throws Exception {
+////        io.couchdrop.endpoints.ssh.SshWorker serverb = new io.couchdrop.endpoints.ssh.SshWorker();
+////        SshServer sshServer = serverb.createSshServer(5025, "", "/tmp", "http://localhost:5088", "fake_authentication_token");
+////        sshServer.start();
+////        while (true) {
+////            Thread.sleep(4000);
+////        }
+////    }
+//}
